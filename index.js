e9ebd3f39c42a72dc8098a3cb7ea7438f9727fcd
Alex Rider <adrider31415@gmail.com>
2025-04-23 16:23:55 -0600

    add button

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-function createGrid(outerDiv, rows, cols, height = 10, width = 10) {
+function createGrid(outerDiv, rows, cols, width = 960) {
     // Create container with column direction
     const container = document.createElement('div');
-    container.style.cssText = `display: flex; flex-direction: column;`;
+    container.style.cssText = `display: flex; flex-direction: column; width: ${width}px; height: ${width}px; justify-content: space-evenly`;
     outerDiv.appendChild(container);
     
     // Create rows
     for (let i = 0; i < rows; i++) {
         const rowDiv = document.createElement('div');
-        rowDiv.style.cssText = `display: flex; flex-direction: row;`;
+        rowDiv.style.cssText = `display: flex; flex-direction: row; justify-content: space-evenly; flex: 1`;
         container.appendChild(rowDiv);
         
         // Create cells in each row
         for (let j = 0; j < cols; j++) {
             const cell = document.createElement('div');
-            cell.style.cssText = `height: ${height}px; width: ${width}px; background-color: blue; margin: 1px;`;
+            cell.style.cssText = `background-color: blue; margin: 0px; flex: 1`;
             cell.addEventListener('mouseenter', () => {cell.style.backgroundColor = 'black'})
             rowDiv.appendChild(cell);
         }
@@ -21,6 +21,6 @@ function createGrid(outerDiv, rows, cols, height = 10, width = 10) {
 }
 
 const body = document.querySelector('body');
-createGrid(body, 50, 50); // Create a 5x5 grid
+createGrid(body, 100, 100); // Create a 5x5 grid
 
 
